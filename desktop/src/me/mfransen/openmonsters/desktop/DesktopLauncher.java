f0686b056fd44c3412bb0b2862b9a5230d8aa799
Matt Fransen <mfransen@gmx.com>
2015-11-20 09:55:34 -0600

    Init

---
+++ desktop/src/me/mfransen/openmonsters/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package me.mfransen.openmonsters.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import me.mfransen.openmonsters.OpenMonstersGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new OpenMonstersGame(), config);
+	}
+}
