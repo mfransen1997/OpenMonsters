3f8818b5013dd773c3d2dd1b4ce2533128bf4ba7
Matt Fransen <mfransen@gmx.com>
2015-11-30 18:44:10 -0600

    Added fields to Creature Interface getID() : String getDisplayName() : String

--- AddonLoader/src/me/mfransen/openmonsters/api/addons/me/mfransen/openmonsters/api/ICreature.java
+++ AddonLoader/src/me/mfransen/openmonsters/api/addons/me/mfransen/openmonsters/api/ICreature.java
@@ -4,4 +4,6 @@ package me.mfransen.openmonsters.api.addons.me.mfransen.openmonsters.api;
  * Created by Matt Fransen on 11/30/2015.
  */
 public interface ICreature {
+    String getID();
+    String getDisplayName();
 }
